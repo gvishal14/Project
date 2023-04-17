600ead59e522bb21116c9aa2d6116188c7c78768
gvishal14 <vishalgupta140701@gmail.com>
2023-04-17 10:22:05 +0530

    ..update

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Buttons from '../components/Buttons';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
